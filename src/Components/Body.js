e9a34b1cf84046d341e31af214f7d8c7a016bedc
Weligama49 <it22127228@my.sliit.lk>
2025-04-26 15:23:03 +0530

    feat: recipe add, redipe read

---
+++ src/Components/Body.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function body(){
+    return(
+        <div>
+            <img className="img" src="./Image/1.jpg" alt=""  />
+            </div>
+    )
+};
+export default body;
---
+++ src/Components/Header.js
@@ -0,0 +1,85 @@
+import React from "react";
+import{ useNavigate} from "react-router-dom";
+import "../Components/RecipeStyle.css";
+import Body from '../Components/Body';
+
+
+function Header(){
+
+   
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <p style={{ width:"50%", marginLeft: "27%",marginRight:"20%", marginTop:"1%", color: "rgba(167, 76, 23, 0.93)", fontWeight: "1000" ,fontSize:"35px"}}>
+              Traditional Recipe Sharing & Learning
+            </p>
+
+            
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{color: "red"}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand">
+                        <b>
+                            <h4 style={{ color: "rgba(243, 224, 24, 0.98)" }}>
+                               <b><b>
+                                    Recipe <span style={{ color: "white" }}>Management</span>
+                                </b> </b>
+                            </h4>
+                        </b>
+                    </a>
+                  <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav" style={{ marginLeft: "20%" }}>
+                            <li className="nav-item">
+                                <button className="nav-btn" onClick={() => navigate("/")}>
+                                    {" "}
+                                    <div className="nav-link_H" aria-current="page" href="#"><b>
+                                        Home
+                                        </b>
+                                    </div>
+                                </button>
+                            </li>
+
+                            <li className="nav-item" style={{ marginLeft: "60%" }}>
+                                <button className="nav-btn" onClick={() => navigate("/recipes")}>
+                                    <div className="nav-link_H">
+                                   <b>Post Sharing</b> 
+                                    </div>
+                                </button>
+                            </li>
+
+                            <li className="nav-item" style={{ marginLeft: "60%" }}>
+                                <button className="nav-btn" onClick={() => navigate("/postview")}>
+                                    {" "}
+                                    <div className="nav-link_H">
+                                       <b>Post View </b> 
+                                    </div>
+                                </button>
+                            </li>
+
+                            
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        <div>
+        </div>
+        </div>
+
+
+    ); 
+
+}
+
+export default Header;
